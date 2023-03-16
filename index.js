c6e41d948ac6ec3d59ac066d4bf847d84555004d
Paul Warren Seabrook <paulwarrenseabrook@gmail.com>
2023-03-16 15:20:26 -0400

    complete initial version of skills, projects, and about pages

--- index.js
+++ index.js
@@ -2,11 +2,14 @@ const hamburger = document.querySelector('.hamburger')
 const navMenu = document.querySelector('.nav-menu')
 const skills = document.querySelector('#skills')
 const skillsNav = document.querySelector('#skillsNav')
+const projects = document.querySelector('#projects')
+const about = document.querySelector('#about')
 const home = document.querySelector('#home')
 const arrowToSkills = document.querySelector('.arrowToSkills')
+const arrowToProjects = document.querySelector('.arrowToProjects')
 const arrowToHome = document.querySelector('.arrowToHome')
+const arrowToAbout = document.querySelector('.arrowToAbout')
 
-console.log(arrowToSkills)
 hamburger.addEventListener('click', () => {
   hamburger.classList.toggle('active')
   navMenu.classList.toggle('active')
@@ -27,6 +30,18 @@ arrowToSkills.addEventListener('click', () => {
 
 arrowToHome.addEventListener('click', () => {
   home.scrollIntoView({
+    behavior: 'smooth',
+  })
+})
+
+arrowToProjects.addEventListener('click', () => {
+  projects.scrollIntoView({
+    behavior: 'smooth',
+  })
+})
+
+arrowToAbout.addEventListener('click', () => {
+  about.scrollIntoView({
     behavior: 'smooth',
   })
 })
